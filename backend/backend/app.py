0bf06ada619d85e07115235b4b087bd1591845c8
DEEPAK SINGH <108828387+deepak-2605@users.noreply.github.com>
2024-09-02 21:25:36 +0530

    Update app.py

--- backend/backend/app.py
+++ backend/backend/app.py
@@ -1,6 +1,7 @@
+import os
 from flask import Flask
 from flask_cors import CORS
-from flask import Flask, request, jsonify
+from flask import request, jsonify
 from job_recommender import job_recommender
 
 app = Flask(__name__)
@@ -8,23 +9,19 @@ app = Flask(__name__)
 # Enable CORS for all routes
 CORS(app)
 
-
 @app.route('/recommend', methods=['POST'])
 def recommend_jobs():
     # Parse the incoming JSON request
-    # data = request.json
-    print("request ",request.files)
     file = request.files['resume']
-    print(file)
     file_path = 'temp_resume.pdf'
     file.save(file_path)
     
-    # You can call the recommendation function from your job recommender script here
-    recommendations = job_recommender.getRecommendations(file_path)  # Adjust this function name according to your implementation
+    # Call the recommendation function from your job recommender script
+    recommendations = job_recommender.getRecommendations(file_path)
 
     # Return the recommendations as a JSON response
     return jsonify(recommendations)
-    # return jsonify("HELLO")
-
+ 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Default to port 5000 if PORT is not set
+    app.run(debug=True, port=port)
